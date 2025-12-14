ddf31f02429216fe880554a72679efd37df2dc3e
zia <chinese.zhaoziye@gmail.com>
2025-12-14 21:58:25 +1030

    create project

---
+++ CitySkylineModTemplate/Main.cs
@@ -0,0 +1,11 @@
+
+using ICities;
+
+namespace CitySkylineModTemplate
+{
+  public class Main: IUserMod
+  {
+    public string Name => "CitySkylineModTemplate";
+    public string Description => "MOD_DESCRIPTION";
+  }
+}
